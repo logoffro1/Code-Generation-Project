57f656af6805396022fcce22c8f644ba1de19dce
InhollandEgehan <56778604+InhollandEgehan@users.noreply.github.com>
2021-05-25 01:44:44 +0200

    initializing the server stub for the first time

---
+++ src/main/java/io/swagger/api/UsersApiController.java
@@ -0,0 +1,109 @@
+package io.swagger.api;
+
+import io.swagger.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-24T23:22:40.244Z[GMT]")
+@RestController
+public class UsersApiController implements UsersApi {
+
+    private static final Logger log = LoggerFactory.getLogger(UsersApiController.class);
+
+    private final ObjectMapper objectMapper;
+
+    private final HttpServletRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public UsersApiController(ObjectMapper objectMapper, HttpServletRequest request) {
+        this.objectMapper = objectMapper;
+        this.request = request;
+    }
+
+    public ResponseEntity<User> createUser(@Parameter(in = ParameterIn.DEFAULT, description = "User registered", required=true, schema=@Schema()) @Valid @RequestBody User body) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<User>(objectMapper.readValue("{\n  \"firstName\" : \"john\",\n  \"lastName\" : \"winchester\",\n  \"password\" : \"whatever\",\n  \"phoneNumber\" : \"090078601\",\n  \"role\" : \"employee\",\n  \"id\" : 1,\n  \"creationDate\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"email\" : \"john@gmail.com\"\n}", User.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<User>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<User>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<List<User>> deleteUser(@Min(1L)@Parameter(in = ParameterIn.PATH, description = "User id to get from the database", required=true, schema=@Schema(allowableValues={  }, minimum="1"
+)) @PathVariable("userId") Long userId) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<List<User>>(objectMapper.readValue("[ {\n  \"firstName\" : \"john\",\n  \"lastName\" : \"winchester\",\n  \"password\" : \"whatever\",\n  \"phoneNumber\" : \"090078601\",\n  \"role\" : \"employee\",\n  \"id\" : 1,\n  \"creationDate\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"email\" : \"john@gmail.com\"\n}, {\n  \"firstName\" : \"john\",\n  \"lastName\" : \"winchester\",\n  \"password\" : \"whatever\",\n  \"phoneNumber\" : \"090078601\",\n  \"role\" : \"employee\",\n  \"id\" : 1,\n  \"creationDate\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"email\" : \"john@gmail.com\"\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<List<User>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<List<User>>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<List<User>> getUserById(@Min(1L)@Parameter(in = ParameterIn.PATH, description = "User id to get from the database", required=true, schema=@Schema(allowableValues={  }, minimum="1"
+)) @PathVariable("userId") Long userId) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<List<User>>(objectMapper.readValue("[ {\n  \"firstName\" : \"john\",\n  \"lastName\" : \"winchester\",\n  \"password\" : \"whatever\",\n  \"phoneNumber\" : \"090078601\",\n  \"role\" : \"employee\",\n  \"id\" : 1,\n  \"creationDate\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"email\" : \"john@gmail.com\"\n}, {\n  \"firstName\" : \"john\",\n  \"lastName\" : \"winchester\",\n  \"password\" : \"whatever\",\n  \"phoneNumber\" : \"090078601\",\n  \"role\" : \"employee\",\n  \"id\" : 1,\n  \"creationDate\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"email\" : \"john@gmail.com\"\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<List<User>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<List<User>>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    public ResponseEntity<List<User>> getUsers(@Parameter(in = ParameterIn.QUERY, description = "" ,schema=@Schema()) @Valid @RequestParam(value = "offset", required = false) Integer offset,@Parameter(in = ParameterIn.QUERY, description = "" ,schema=@Schema()) @Valid @RequestParam(value = "limit", required = false) Integer limit) {
+        String accept = request.getHeader("Accept");
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<List<User>>(objectMapper.readValue("[ {\n  \"firstName\" : \"john\",\n  \"lastName\" : \"winchester\",\n  \"password\" : \"whatever\",\n  \"phoneNumber\" : \"090078601\",\n  \"role\" : \"employee\",\n  \"id\" : 1,\n  \"creationDate\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"email\" : \"john@gmail.com\"\n}, {\n  \"firstName\" : \"john\",\n  \"lastName\" : \"winchester\",\n  \"password\" : \"whatever\",\n  \"phoneNumber\" : \"090078601\",\n  \"role\" : \"employee\",\n  \"id\" : 1,\n  \"creationDate\" : \"2000-01-23T04:56:07.000+00:00\",\n  \"email\" : \"john@gmail.com\"\n} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<List<User>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<List<User>>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+}
