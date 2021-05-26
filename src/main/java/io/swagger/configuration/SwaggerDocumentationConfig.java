ed55ebc52e43f3913a4b17d80b4a4a0fd579859f
egecnr <54850459+egecnr@users.noreply.github.com>
2021-05-26 02:33:28 +0200

    Some plugins are creating problem... But the general configuration is done

--- src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -12,6 +12,7 @@ import springfox.documentation.service.ApiInfo;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import java.util.function.Predicate;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2021-05-26T00:05:39.615Z[GMT]")
 @Configuration
