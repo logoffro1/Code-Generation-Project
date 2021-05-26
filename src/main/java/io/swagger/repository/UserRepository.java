b053cd3a638a1150d9b3d4c471f1c3ace24d6a51
Cosmin Ilie <cosmin.ilie2704@gmail.com>
2021-05-26 23:36:37 +0200

    deleted everything

--- src/main/java/io/swagger/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package io.swagger.repository;
-
-import io.swagger.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User,Long> {
-}
